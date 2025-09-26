1726c3330446dc0a339d6457dfe20e63d454c400
flavioscipione00 <flavio.scipione.00@gmail.com>
2025-09-26 15:11:34 +0200

    Update mcsim.cpp

--- mcsim.cpp
+++ mcsim.cpp
@@ -1,16 +1,23 @@
 #include "./sim.hpp"
 #include <fstream>
-#include <chrono>
+#include <chrono>  //useful to check times 
 
 int main(int argc, char **argv)
 {
 
-
    mcsim<monomer> mc;
+
+//setting the Random Number Generator to a casual seed
    mc.init_rng(-1);
 
+//setting  the stating configuration:
+// 1) assign per particle properties
+// 2) seed and grow the polymers on a SC lattice
+
    mc.prepare_initial_conf();
-   mc.run(15);
+
+//starting the run, 0 is the suffix of the ".txt" snapshot files
+   mc.run(0);
 
   
   return 0;
